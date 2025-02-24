24ca3009dd31402d8a7856af815e0cfdbf2809a6
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-02-24 10:26:19 +0000

    Dependency injection by static instantiation

---
+++ src/main/java/presentation/Presentation_v1.java
@@ -0,0 +1,18 @@
+package presentation;
+
+import dao.DaoImpl;
+import dao.IDao;
+import metier.IMetier;
+import metier.MetierImpl;
+
+public class Presentation_v1 {
+    public static void main(String[] args) {
+        /*
+        Injection des dépendences par instanciation statique
+         */
+        DaoImpl dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao); // Injection via le setter
+        System.out.println("Resultat = "+metier.calcul());
+    }
+}
