66de1ed5d5724657ef4fe8189de2dd6f18b67469
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-02-24 10:10:48 +0000

    Create Interface metier implementation

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,23 @@
+package metier;
+
+import dao.DaoImpl;
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+
+    // Couplage faible
+    private IDao dao;
+
+    public void setDao(IDao dao) {
+        /**
+         * Pour injecter la variable dao un objet d'une classe qui implémente l'interface IDao
+         */
+    }
+
+    @Override
+    public double calcul() {
+        double temp = dao.getData();
+        double res = temp*23
+        return 0;
+    }
+}
