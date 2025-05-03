5dd1c6366135a89dacf6980e185936151b7cc435
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-04 00:55:37 +0100

    Dynamic Instantiation v2

--- src/main/java/presentation/Presentation_v2.java
+++ src/main/java/presentation/Presentation_v2.java
@@ -1,7 +1,12 @@
 package presentation;
 
+import dao.DaoImpl;
 import dao.IDao;
+import metier.IMetier;
+import metier.MetierImpl;
+
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Presentation_v2 {
@@ -9,11 +14,24 @@ public class Presentation_v2 {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(new File("config.txt"));
+//            DaoImpl dao = new DaoImpl();
             String daoClassName = scanner.nextLine();
             Class cDao = Class.forName(daoClassName);
             IDao dao = (IDao) cDao.getConstructor().newInstance();
-            System.out.println(dao.getData());
-        } catch (Exception e) {
+//            System.out.println(dao.getData());
+
+//            MetierImpl metier = new MetierImpl(dao);
+            String metierClassName = scanner.nextLine();
+            Class cMetier = Class.forName(metierClassName);
+            IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+            // metier.setDao(dao);
+            Method setDao = metier.getClass().getMethod("setDao", IDao.class);
+            setDao.invoke(metier, dao);
+
+            System.out.println("Res = " + metier.calcul());
+
+        } catch (Exception e ) {
             System.out.println(e.getMessage());
         }
     }
