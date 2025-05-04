bfe7a67321b13d3102ae8d7ec767f98ae6663b12
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-04 02:33:48 +0100

    Dependency injection using spring annotation version injection

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -2,14 +2,15 @@ package metier;
 
 import dao.DaoImpl;
 import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("metier")
 public class MetierImpl implements IMetier {
 
-    // Couplage faible
+    @Autowired
     private IDao dao;
 
-    public MetierImpl() {
-    }
 
     public MetierImpl(IDao dao) {
         this.dao = dao;
